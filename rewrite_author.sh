2ee62f21060f684cb3eb85685bde19d3b74165ad
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-06-24 19:52:14 +0200

    refactor: update rewrite_author.sh

--- rewrite_author.sh
+++ rewrite_author.sh
@@ -3,24 +3,26 @@
 #
 # Licensed under MIT - see https://github.com/MichaelCurrin/rewrite-git-author/blob/main/LICENSE .
 
-if [[ $# -ne 2 ]]; then
+if [[ "$#" -ne 2 ]]; then
   echo "Usage: $0 OLD_EMAIL NEW_EMAIL"
-  exit 1
+  exit 0
 fi
 
-VARS="
 OLD_EMAIL=$1
 NEW_EMAIL=$2
+
+VARS="
+OLD_EMAIL=$OLD_EMAIL
+NEW_EMAIL=$NEW_EMAIL
 "
 
 CONDITION='
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]; then
+  export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]; then
+  export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
 fi
 '
 
@@ -28,4 +30,10 @@ FILTER="$VARS $CONDITION"
 
 printf "$FILTER\n"
 
-git filter-branch -f --env-filter "$FILTER" --tag-name-filter cat -- --branches --tags
+git filter-branch \
+  -f \
+  --env-filter "$FILTER" \
+  --tag-name-filter \
+  cat -- \
+  --branches \
+  --tags
